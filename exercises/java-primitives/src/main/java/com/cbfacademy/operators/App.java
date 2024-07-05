130db17226b9a074aad547f1cd53234fc1389f70
Arin Ososanya <arin.ososanya@gmail.com>
2024-07-05 13:08:30 +0100

    CBF Coding Activities - - Strings - Operators - QuickSort - Exceptions (not yet finished)

--- exercises/java-primitives/src/main/java/com/cbfacademy/operators/App.java
+++ exercises/java-primitives/src/main/java/com/cbfacademy/operators/App.java
@@ -6,26 +6,50 @@ public class App {
     }
 
     public static double add(double operand1, double operand2) {
-        throw new RuntimeException("Not implemented");
+        return operand1 + operand2;
     }
 
     public static double subtract(double operand1, double operand2) {
-        throw new RuntimeException("Not implemented");
+        return operand1 - operand2;
     }
 
     public static double multiply(double operand1, double operand2) {
-        throw new RuntimeException("Not implemented");
+
+        return operand1 * operand2;
     }
 
     public static Boolean areEqual(double operand1, double operand2) {
-        throw new RuntimeException("Not implemented");
+
+        // We don't use equals(operand2) because because the equals method of primitive data types like double doesn't directly compare floating-point numbers for equality.
+        return operand1 == operand2;
     }
 
     public static Boolean isLessThan(double operand1, double operand2) {
-        throw new RuntimeException("Not implemented");
+        return operand1<operand2;
     }
 
     public static Boolean isMoreThan(double operand1, double operand2) {
-        throw new RuntimeException("Not implemented");
+        return operand1>operand2;
     }
 }
+// Unary Operations
+// If we have this input:
+//int number1 = 12;
+//int number2 = 12;
+//System.out.println(number1++);
+//System.out.println(++number2);
+// We get out:
+//12 and
+//13
+//
+//This is because (number1++) number1 has the value 12 initially.
+//Since it's a postfix increment, the expression first evaluates to the current value of number1 (which is 12).
+//Then, number1 is incremented by 1 (so it becomes 13).
+//However, the increment happens after the value is used in the expression, so the System.out.println statement prints the original value (12).
+// Meanwhile, (++number2) is a prefix increment.
+//The value of number2 (which is 12) is incremented by 1 before it's used in the expression.
+//So, number2 becomes 13.
+//Now, the expression evaluates to the incremented value of number2 (which is 13).
+//Therefore, the System.out.println statement prints 13.
+
+// All activities complete? (Y)
--- exercises/java-primitives/src/main/java/com/cbfacademy/strings/App.java
+++ exercises/java-primitives/src/main/java/com/cbfacademy/strings/App.java
@@ -1,3 +1,4 @@
+// ACTIVITY COMPLETE
 package com.cbfacademy.strings;
 
 public class App {
@@ -6,17 +7,26 @@ public class App {
     }
 
     public static String concatenate(String word1, String word2, String word3) {
-        // TODO: Write code that concatenates the input parameters and returns the result
-        throw new RuntimeException("Not implemented");
+        // TODO: Write code that concatenates the input parameters and returns the result (DONE)
+        return word1.concat(word2).concat(word3);
+
     }
 
     public static Boolean areEqual(String word1, String word2) {
-        // TODO: Write code to determine whether the input parameters are equal strings
-        throw new RuntimeException("Not implemented");
+        // TODO: Write code to determine whether the input parameters are equal strings (DONE)
+        return word1.equals(word2);
+
     }
 
     public static String format(String item, int quantity, double price) {
-        // TODO: Write code to return a string formatted as follows: "Item: [item]. Price: £[amount]. Quantity: [quantity]". The date should be formatted as DD/MM/YYYY, e.g. 24/10/2023
-        throw new RuntimeException("Not implemented");
+        // TODO: Write code to return a string formatted as follows: "Item: [item]. Price: £[amount]. Quantity: [quantity]". The date should be formatted as DD/MM/YYYY, e.g. 24/10/2023 (DONE)
+        return "Item: " + item + ". Price: £" + String.format("%.2f", price) + ". Quantity: " + quantity;
     }
+
 }
+
+// For the formatted String activity above, we did the following:
+// 1. We declare a variable formattedString to hold the final output.
+// 2. We use string concatenation to build the string with placeholders for item, formatted price, and quantity.
+// 3. For the price, we use String.format("%.2f", price) within the string to format the double value price as a String with two decimal places and a leading £ symbol. NOTE: The %.2f format specifier ensures two decimal places.
+// We don't use the '%.2d' format because then it won't show decimal places, it shows as an integer but we want 2 decimal places.
