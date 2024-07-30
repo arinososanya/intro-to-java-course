17ca2556f819cb1795a3b3e34f9f4911c8cc814c
Arin Ososanya <arin.ososanya@gmail.com>
2024-07-30 19:44:16 +0100

    Decoupling: Replacing statements in DiceGame and App as per activity

--- exercises/software-design/src/main/java/com/cbfacademy/App.java
+++ exercises/software-design/src/main/java/com/cbfacademy/App.java
@@ -2,7 +2,7 @@ package com.cbfacademy;
 
 public class App {
     public static void main(String[] args) {
-        DiceGame game = GameFactory.create(); // Replace the new DiceGame() statement in App with GameFactory.create()
+        DiceGame game = GameFactory.create(); // Replace the new DiceGame() statement in App with GameFactory.create(). This delegated responsibility to the factory allows us to decouple the DiceGame class from the DicePlayer class.
         String winner = game.play();
 
         if (winner == null) {
