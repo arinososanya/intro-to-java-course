508c42a7816b26ab02cd8a15bed6cf56d6d02090
Arin Ososanya <arin.ososanya@gmail.com>
2024-08-29 18:32:28 +0100

    Car.java exercise 1 complete

--- exercises/object-oriented-programming/cars/src/main/java/com/cbfacademy/cars/Car.java
+++ exercises/object-oriented-programming/cars/src/main/java/com/cbfacademy/cars/Car.java
@@ -1,28 +1,45 @@
 package com.cbfacademy.cars;
 
 public class Car {
+
+    private String make;
+    private String model;
+    private int year;
+    private String colour;
+
+    // Constructor
     public Car(String make, String model, String colour, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.colour = colour;
     }
 
-    public String getMake() {
-        return "";
-    }
+        // Getter methods. These methods access and return values of fields
+        public String getMake() {
+            return this.make;
+        }
 
-    public String getModel() {
-        return "";
-    }
+        public String getModel() {
+            return this.model;
+        }
 
-    public int getYear() {
-        return 0;
-    }
+        public int getYear () {
+            return this.year;
+        }
 
-    public String getColour() {
-    }
+        public String getColour () {
+            return this.colour;
 
-    public void setColour(String newColour) {
-    }
+        }
 
-    public String getDetails() {
-        return null;
-    }
+// Setter Method
+        public void setColour(String colour) {
+            this.colour = colour;
+        }
+
+        // Instantiate three Car objects. The new Car object would typically go in the main method or another method where you want to create and use the object.
+        public String getDetails(){
+            return "Car Details" + this.make + this.model + this.colour + this.year; // Returns the car details, including the make, model, colour and year
+        }
 }
